aa7d1ac7c77b1a573f75122e6862495723065216
Osama Rimawi <osamarimawi2002@gmail.com>
2024-03-10 01:14:26 +0200

    Create Product and Inventory classes, with properties and constructors

---
+++ Source Code/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System
+{
+    public class Inventory
+    {
+        public List<Product> Products { get; set; }
+        public Inventory() {
+            Products = [];
+        }
+        public Inventory(List<Product> products)
+        {
+            Products = products;
+        }
+
+    }
+}
---
+++ Source Code/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System
+{
+    public class Product(string name, double price, int quantity)
+    {
+        public string Name { get; set; } = name;
+        public double Price { get; set; } = price;
+        public int quantity { get; set; } = quantity;
+
+
+    }
+}
